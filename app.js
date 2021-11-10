030c2c1ff0a35a8b409de67697b40fa5f22e1a70
MimimiBB-8 <yliame14@gmail.com>
2021-11-10 17:07:54 +0300

    fix function

--- app.js
+++ app.js
@@ -1,50 +1,57 @@
 const addInput = document.getElementById('input_task');
 const addButton = document.getElementById('add_task_btn');
 const todosWrapper = document.getElementById('todo_wrapper');
-var countTodoTask = 0;
+const todo = document.querySelector('.todo');
 
-let task = [];
 let todoItems = [];
+let task = [];
+
 function Task(description) {
   this.description = description;
   this.state = false;
 }
 
-const CompliteTask = (index, elem) => {
-  task[index].state = !task[index].state;
-  if (task[index].state) {
-    todoItems[index].classList.add('checked');
-  }
-  else {
-    todoItems[index].classList.remove('checked');
-  }
-}
-
 const createTemplate = (task, index) => {
   return `
-    <div class='row row-justify-content-between align-self-stretch add_item ${task.state ? 'checked' : ''}' id='add_item_id'>
-    <div class="col col-1">
-    <input class="form-check-input btn_state" type="checkbox" value=""  onclick='CompliteTask(${index})' >
+    <li class='add_item' onclick='compliteTask(event, ${index})'  ${task.state ? 'checked' : ''}>
+    <div class='descriptions_wrapper'>
+    <input class="btn_state" type="checkbox" id='item_${index}'  ${task.state ? 'checked' : ''}>
+    <label for='item_${index}' class="descriptions">${task.description}</label>
     </div>
-    <div class="col col-4">
-    <div class="descriptions">${task.description}</div>
-    </div>
-    <div class="col col-1">
-    <button class='btn_delete btn btn-outline-dark' onclick='deleteTask(${index})'>Delete</button>
-    </div>
-      
-    </div>  
+    <button class='btn btn_delete ' onclick='deleteTask(${index})'>Delete</button>
+    </li>  
   `
+}
+
+const compliteTask = (event, index) => {
+  if (event.target.nodeName == 'INPUT') {
+    task[index].state = !task[index].state;
+    if (task[index].state) {
+      todoItems[index].classList.add('checked');
+    }
+    else {
+      todoItems[index].classList.remove('checked');
+    }
+  }
+  addElement();
+}
+
+const filterTask = () => {
+  const activeTask = task.filter(item => item.state == false);
+  const doneTask = task.filter(item => item.state == true);
+  task = [...activeTask, ...doneTask]
 }
 
 const addElement = () => {
   todosWrapper.innerHTML = "";
   if (task.length > 0) {
+    filterTask();
     task.forEach((item, index) => {
       todosWrapper.innerHTML += createTemplate(item, index);
     })
     todoItems = document.querySelectorAll('.add_item')
   }
+
 }
 
 const deleteTask = index => {
@@ -53,14 +60,22 @@ const deleteTask = index => {
 }
 
 addButton.addEventListener('click', () => {
+  actionInput();
+})
+
+addInput.addEventListener('keypress', function (e) {
+  if (e.key === 'Enter') {
+    actionInput();
+  }
+})
+
+const actionInput = () => {
   if (addInput.value.length > 0) {
     task.push(new Task(addInput.value));
     addElement();
     addInput.value = '';
-
   }
   else {
     alert('Please, write your task')
   }
-})
-
+}
